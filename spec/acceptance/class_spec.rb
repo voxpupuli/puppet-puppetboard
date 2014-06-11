597ba9a34b1d8265cc1c2f41952139167b1b4d3f
Spencer Krum <nibz@cat.pdx.edu>
2014-06-11 13:38:10 -0700

    more science

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper_acceptance'
-require 'pry'
 
 describe 'puppetboard class' do
 
@@ -12,6 +11,7 @@ describe 'puppetboard class' do
           on host, "wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm; rpm -ivh epel-release-6-8.noarch.rpm"
         end
       end
+      on host, "puppet module install puppetlabs/apache"
       install_package host, 'python-virtualenv'
       install_package host, 'git'
     end
@@ -25,11 +25,57 @@ describe 'puppetboard class' do
     end
 
     it 'should not answer to localhost' do
-      shell("/usr/bin/curl localhost:80", :acceptable_error_codes => 7) do |r|
+      shell("/usr/bin/curl localhost:80", :acceptable_exit_codes => 7) do |r|
         r.exit_code.should == 7 # curl (7): Couldn't connect to host
       end
     end
 
-    #binding.pry
+
+  end
+
+  context 'default parameters' do
+    hosts.each do |host|
+      if fact('osfamily') == 'RedHat'
+        if fact('architecture') == 'amd64'
+          on host, "wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm; rpm -ivh epel-release-6-8.noarch.rpm"
+        else
+          on host, "wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm; rpm -ivh epel-release-6-8.noarch.rpm"
+        end
+      end
+      on host, "puppet module install puppetlabs/apache"
+      install_package host, 'python-virtualenv'
+      install_package host, 'git'
+    end
+
+    it 'should work with no errors' do
+      pp= <<-EOS
+      # Configure Apache on this server
+      class { 'apache':
+        default_vhost => false,
+        purge_configs => true,
+      }
+      class { 'apache::mod::wsgi': }
+
+      # Configure Puppetboard
+      class { 'puppetboard': }
+
+      # Access Puppetboard through pboard.example.com
+      class { 'puppetboard::apache::vhost':
+        vhost_name => 'pboard.example.com',
+      }
+      EOS
+
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    it 'should answer to localhost' do
+      shell("/usr/bin/curl localhost:80") do |r|
+        r.stdout.should =~ /niele Sluijters/
+        r.exit_code.should == 0
+      end
+    end
   end
 end
