f36fc0e9ba7d33292c664bc15fe6089af008e72f
Wolf Noble <wnoble@rmn.com>
2014-06-18 15:51:35 -0500

    move custom settings into settings.py. remove management of default_settings.py

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,17 +25,18 @@ class puppetboard::params {
   $puppetdb_host = 'localhost'
   $puppetdb_port = 8080
   $puppetdb_key = 'None'
-  $puppetdb_ssl = 'False'
+  $puppetdb_ssl = false
   $puppetdb_cert = 'None'
   $puppetdb_timeout = 20
   $dev_listen_host = '127.0.0.1'
   $dev_listen_port = 5000
   $unresponsive = 3
-  $enable_query = 'True'
-  $localise_timestamp = 'True'
+  $enable_query = true
+  $localise_timestamp = true
   $python_loglevel = 'info'
   $python_proxy = false
-  $experimental = 'False'
+  $reports_count = '10'
+  $experimental = false
   $revision = undef
   $virtualenv = 'python-virtualenv'
 }
