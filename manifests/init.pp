voxpupuli/puppet-puppetboard

commit 6b95a6ae53e7c4fc74ae830fe4123e3fdf7cb15b

update documentation to reflect puppetdb_ssl is supposed to be a boolean and not a string

manifests/init.pp
@@ -44,8 +44,8 @@
 #   Defaults to 'None' ($::puppetboard::params::puppetdb_key)
 #
 # [*puppetdb_ssl_verify*]
-#   (string) whether PuppetDB uses SSL or not,  'True' or 'False', or the path to the puppet CA
-#   Defaults to 'False' ($::puppetboard::params::puppetdb_ssl_verify)
+#   (string) whether PuppetDB uses SSL or not (true or false)
+#   Defaults to false ($::puppetboard::params::puppetdb_ssl_verify)
 #
 # [*puppetdb_cert*]
 #   (string, absolute path) path to PuppetMaster/CA signed client SSL cert
@@ -170,6 +170,7 @@ class puppetboard(
   validate_bool($experimental)
   validate_bool($localise_timestamp)
   validate_hash($extra_settings)
+  validate_bool($puppetdb_ssl_verify)
 
   if $manage_group {
     group { $group:

commit 0ebb5a454d4f9bc133065e11bb7f5cb32177f9a2

Fixing what was broken in: 6b95a6ae53e7c4fc74ae830fe4123e3fdf7cb15b This should resolve the following issues: https://github.com/voxpupuli/puppet-module-puppetboard/issues/94 https://github.com/voxpupuli/puppet-module-puppetboard/issues/65 (not 100% sure on this one)

manifests/init.pp
@@ -44,7 +44,7 @@
 #   Defaults to 'None' ($::puppetboard::params::puppetdb_key)
 #
 # [*puppetdb_ssl_verify*]
-#   (string) whether PuppetDB uses SSL or not (true or false)
+#   (bool, string) whether PuppetDB uses SSL or not (true or false), or the path to the puppet CA
 #   Defaults to false ($::puppetboard::params::puppetdb_ssl_verify)
 #
 # [*puppetdb_cert*]
@@ -170,7 +170,11 @@ class puppetboard(
   validate_bool($experimental)
   validate_bool($localise_timestamp)
   validate_hash($extra_settings)
-  validate_bool($puppetdb_ssl_verify)
+  if $puppetdb_ssl_verify {
+    unless is_string($puppetdb_ssl_verify) {
+      validate_bool($puppetdb_ssl_verify)
+    }
+  }
 
   if $manage_group {
     group { $group:

templates/settings.py.erb
@@ -13,8 +13,10 @@ PUPPETDB_HOST = '<%= @puppetdb_host %>'
 <% if @puppetdb_port -%>
 PUPPETDB_PORT = <%= @puppetdb_port %>
 <% end -%>
-<% if @puppetdb_ssl_verify -%>
+<% if @puppetdb_ssl_verify.is_a? String -%>
 PUPPETDB_SSL_VERIFY = '<%= @puppetdb_ssl_verify %>'
+<% elsif @puppetdb_ssl_verify -%>
+PUPPETDB_SSL_VERIFY = True
 <% else -%>
 PUPPETDB_SSL_VERIFY = False
 <% end -%>