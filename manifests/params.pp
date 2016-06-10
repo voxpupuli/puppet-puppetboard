ea1c1be446fdf22d023ce6e1b7b0d2de50fee124
Steve Ames <sames@arxan.com>
2016-06-10 15:40:11 -0400

    Revert parts of the previous commit as I inadvertantly removed two lines

--- manifests/params.pp
+++ manifests/params.pp
@@ -54,6 +54,7 @@ class puppetboard::params {
   $enable_catalog = false
   $enable_query = true
   $localise_timestamp = true
+  $offline_mode = false
   $python_loglevel = 'info'
   $python_proxy = false
   $reports_count = '10'
@@ -62,5 +63,6 @@ class puppetboard::params {
   $virtualenv = 'python-virtualenv'
   $listen = 'private'
   $apache_override = 'None'
+  $default_environment = 'production'
   $extra_settings = {}
 }
