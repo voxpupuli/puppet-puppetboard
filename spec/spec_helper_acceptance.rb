86cb848489de6b74cecb793f3a38fa252261a908
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-19 15:22:51 +0200

    Clean up acceptance spec helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,15 +3,19 @@ require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
-install_module
-install_module_dependencies
-
-# Install additional modules for soft deps
-install_module_from_forge('puppetlabs-apache', '>= 2.1.0 < 6.0.0')
-install_module_from_forge('stahnma-epel', '>= 1.2.2 < 2.0.0')
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    install_module
+    install_module_dependencies
+
+    # Install additional modules for soft deps
+    install_module_from_forge('puppetlabs-apache', '>= 2.1.0 < 6.0.0')
+    install_module_from_forge('stahnma-epel', '>= 1.2.2 < 2.0.0')
+  end
 end
