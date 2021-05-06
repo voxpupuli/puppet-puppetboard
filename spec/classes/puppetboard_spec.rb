88be4f06908d710e2bd866725f4bc5a15e6471d9
Romain Tartière <romain@blogreen.org>
2021-05-05 14:07:22 -1000

    Fix the way facts are set in the test suite
    We must set the facts in the context of a specific operating system,
    otherwise all operating systems will share the same facts.
    
    This is currently not a problem because only Debian and Ubuntu are
    supported and tested against, and Ubuntu being a Debian derivative both
    OS facts are interchangeable, but adding support for more operating
    systems trigger errors (e.g. FreeBSD).

--- spec/classes/puppetboard_spec.rb
+++ spec/classes/puppetboard_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'puppetboard', type: :class do
   on_supported_os.each do |os, facts|
-    let :facts do
-      facts
-    end
-
     context "on #{os}" do
+      let :facts do
+        facts
+      end
+
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('puppetboard') }
       it { is_expected.to contain_file('/srv/puppetboard/puppetboard/settings.py') }
