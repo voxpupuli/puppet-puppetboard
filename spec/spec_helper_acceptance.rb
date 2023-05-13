b01dad98a2c56af5012fe0ed3ec70252c5246d22
Romain Tartière <romain@blogreen.org>
2023-05-12 15:48:20 -1000

    Reorder soft modules
    It looks like ordering matters: when installing puppetlabs-puppetdb
    after puppetlabs-apache, it takes almost 3 hours to install version
    1.0.3 which is indeed too old to work with the module.
    
    Installing in reverse order seems to help.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,7 +4,7 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
   # Install additional modules for soft deps
-  install_puppet_module_via_pmt_on(host, 'puppetlabs-apache')
   install_puppet_module_via_pmt_on(host, 'puppetlabs-puppetdb')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-apache')
   install_puppet_module_via_pmt_on(host, 'puppet-epel')
 end
