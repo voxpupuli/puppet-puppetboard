e4d197a7d009c88d69a399d5575e045bca0a4c7a
Sascha Rüssel <ruessel@synyx.de>
2023-06-21 13:05:17 +0200

    allow python_version > 3.9

--- manifests/init.pp
+++ manifests/init.pp
@@ -60,7 +60,7 @@
 class puppetboard (
   Stdlib::Absolutepath $apache_confd,
   String[1] $apache_service,
-  Pattern[/^3\.\d$/] $python_version,
+  Pattern[/^3\.\d+$/] $python_version,
   Enum['package', 'pip', 'vcsrepo'] $install_from             = 'pip',
   Boolean $manage_selinux                                     = pick($facts['os.selinux.enabled'], false),
   String $user                                                = 'puppetboard',
