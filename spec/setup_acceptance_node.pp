e6c227a44e7788e7bac7ebd1d710a2155155e5cf
Tim Meusel <tim@bastelfreak.de>
2025-12-05 13:29:43 +0100

    CI: delete legacy setup helper
    For Facter 3 / Puppet 7, we had to install lsb tools. That's not
    required anymore.

--- spec/setup_acceptance_node.pp
+++
@@ -1,8 +0,0 @@
-if $facts['os']['name'] == 'Ubuntu' {
-  # Needed for facter to fetch facts used by the postgresql module
-  if versioncmp($facts['facterversion'], '4.0.0') <= 0 {
-    package{ 'lsb-release':
-      ensure => present,
-    }
-  }
-}
